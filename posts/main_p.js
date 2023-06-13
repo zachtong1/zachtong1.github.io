fb11ba08d40be82a3ab2f50ff00e8bb972fc8db8
Zach Tong <zachtong1@gmail.com>
2023-06-12 23:06:27 -0400

    6/12

--- posts/main_p.js
+++ posts/main_p.js
@@ -1,41 +1,71 @@
-function errorMessage() {
-    alert("Page does not exist at the moment.");
-}
+var caterpied = 0;
+var clicked = 0;
+var picNum = 0;
+var exit = 0;
 
-var clickedAmpharosCount = 0;
-function clickedAmpharos() {
-
-    if (clickedAmpharosCount<10) {
-        clickedAmpharosCount+=1;
-        var audio = new Audio("../sounds/ampharos_noise.mp3");
-        audio.play();
-        var hello_text = document.querySelector(".hello_text");
-        hello_text.classList.remove("invisible_hi");
-        setTimeout(function() {
-            hello_text.classList.add("invisible_hi");
-        }, 300);
-        const options = ["hi.","hi.","hi.","hi.","hi.","hi.","hi.","hi.","yo.","sup"];
-        const randomIndex = Math.floor(Math.random() * options.length);
-        const newText = options[randomIndex];
-        hello_text.textContent = newText;
-    }
-    else if (clickedAmpharosCount==10) {
-        clickedAmpharosCount+=1;
+function caterpie() {
+    event.preventDefault();
+    if (clicked==0 && caterpied==0) {
         var header_image = document.querySelector(".header_image");
         header_image.src = "../caterpie.png";
         header_image.classList.remove("clickable");
         var audio = new Audio("../sounds/caterpie_noise.mp3");
         audio.play();
+        caterpied = 1;
     }
 }
 
-function fadeInAnimation() {
-    var timeDelay = 200; /*millisec*/
-    for (let i=1; i<=f_number; i++) {
-        setTimeout(function() {
-            var f_target_element = document.querySelector(".f"+i);
-            f_target_element.classList.replace("not-visible","fade-in");
-        },i*timeDelay);
+function danceClick() {
+    var audio = document.getElementById("danceMusic");
+    if (clicked==0 && caterpied==0) {
+        clicked=1;
+        audio.play();
+    }
+    else {
+        clicked=0;
+        audio.pause();
     }
 }
 
+function dance() {
+    exit=0;
+    if (clicked==1) {
+        var header_image = document.querySelector(".header_image");
+        
+        if (picNum==0 && exit==0) {
+            picNum=1;
+            exit=1;
+            header_image.src = "../tall1.png";
+            var hello_text = document.querySelector(".mNote1");
+            hello_text.classList.remove("invisible_hi");
+            setTimeout(function() {
+                hello_text.classList.add("invisible_hi");
+            }, 70);
+        }
+        if (picNum==1 && exit==0) {
+            picNum=2;
+            exit=1;
+            header_image.src = "../tall.png";
+        }
+        if (picNum==2 && exit==0) {
+            picNum=3;
+            exit=1;
+            header_image.src = "../tall2.png";
+            var hello_text = document.querySelector(".mNote2");
+            hello_text.classList.remove("invisible_hi");
+            setTimeout(function() {
+                hello_text.classList.add("invisible_hi");
+            }, 70);
+        }
+        if (picNum==3 && exit==0) {
+            picNum=0;
+            exit=1;
+            header_image.src = "../tall.png";
+        }
+
+    }
+
+    
+}
+
+setInterval(dance, 150);
